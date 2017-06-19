7d943e2a5080884541bab058e5dd54dfcde544fd
Anatoliy A Aksenov <anatoliy@aksenov.org>
2017-06-19 14:50:03 +0300

    Adding check document

--- public/app/js/check.js
+++ public/app/js/check.js
@@ -11,7 +11,8 @@
       scope:{
       },
       templateUrl: '/templates/check.html',
-      controller: Check
+      controller: Check,
+      link: Link
     };
 
     return directive;
@@ -25,29 +26,41 @@
     $scope.test = {};
 
     $scope.init = function(){
-      var test = $window.web3.eth.contract([{"constant":true,"inputs":[],"name":"decrypt","outputs":[{"name":"r","type":"uint256"},{"name":"m","type":"uint256"},{"name":"mm","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"msg","type":"uint256"},{"name":"e","type":"uint256"},{"name":"m","type":"uint256"}],"name":"func","outputs":[{"name":"r","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"msg","type":"uint256"},{"name":"e","type":"uint256"},{"name":"m","type":"uint256"}],"name":"decrypt","outputs":[{"name":"r","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"msg","type":"uint256"},{"name":"e","type":"uint256"},{"name":"m","type":"uint256"}],"name":"ecrypt","outputs":[{"name":"r","type":"uint256"}],"payable":false,"type":"function"}]).at("0x8e22ce204b494cf793fc531aa758ec69e04d0586");
       
-      test.ecrypt.call(65, 17, 3233, (err, result) => {
-        console.log(`
-        err = ${JSON.stringify(err)}
-        result = ${JSON.stringify(result)}
-        `);
-      });
-
-      $scope.test = test;
-      
-
     };
 
-    $scope.pay = function(){
-      $scope.test.func.sendTransaction(65, 17, 3233, (err, result) => {
-        console.log(`
-        err = ${JSON.stringify(err)}
-        result = ${JSON.stringify(result)}
-        `);
-      });
-
+    $scope.check = function(){
+       let document = $scope.$parent.contracts.document.contract;
+       let sha256 = $window.CryptoJS.SHA256($scope.file.data);
+       document.agrStatus.call(sha256, (err, data) => {
+        console.log(data);
+       });
     }
+    
+  }
+
+  Link.$inject = ['$scope', '$element'];
+
+  function Link($scope, $element){
+
+    $element.find('#check_file').change((e) => {
+      
+      $scope.file = {};
+      
+      $scope.file.filename = e.target.files[0].name;
+      console.log($scope.file.filename);
+
+      var reader = new FileReader();
+
+      reader.onload = function(e){
+        const f = btoa(e.target.result);
+        console.log(f.substr(0, 100));
+        $scope.file.data = f;
+
+      };
+
+      reader.readAsDataURL(e.target.files[0]);
+    });
   }
 
 })();	
