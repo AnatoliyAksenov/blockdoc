5f88a276ffab5b7ccead405a673813633a36cb3b
Anatoliy A Aksenov <anatoliy@aksenov.org>
2017-06-19 07:33:25 +0300

    Adding upload file for create action

--- public/app/js/create.js
+++ public/app/js/create.js
@@ -11,7 +11,8 @@
       scope:{
       },
       templateUrl: '/templates/create.html',
-      controller: Create
+      controller: Create,
+      link: Link
     };
 
     return directive;
@@ -24,6 +25,27 @@
     $scope.init = function(){
       
     }
+  }
+
+  Link.$inject = ['$scope', '$element'];
+
+  function Link($scope, $element){
+
+    $element.find('#create_file').change((e) => {
+      
+      console.log(e.target.files);
+      var reader = new FileReader();
+      reader.onload = function(e){
+        console.log(btoa(e.target.result));
+      };
+
+      reader.readAsDataURL(e.target.files[0]);
+    });
+    
+    $element.find('#test').on('show.bs.modal', function(e){
+
+      $scope.$digest();
+    });
   }
 
 })();	
